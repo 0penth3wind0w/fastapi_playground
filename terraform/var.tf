55e8aefa46680ce40a8e1f23124f70d8b92a9982
Yanlin Chen <yanlin_chen@trendmicro.com>
2022-04-12 11:47:06 +0800

    [terraform] Remove API gateway resources from main, add variable

---
+++ terraform/var.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  description = "The project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "aws_account_id" {
+  description = "The AWS account id"
+  type        = string
+}
